1fa81e1febdafb3657ef2bc1d4f37c936bf5b926
Andrei Beniash <106547649+Andrew-Beniash@users.noreply.github.com>
2024-09-06 08:46:43 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+while True:
+    user_action = input("Type add, edit, exit, complete or show:")
+    match user_action.strip():
+        case 'add':
+            todo = input("Enter a todo: ") + "\n"
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todos.append(todo)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        case 'show':
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            new_todos = [item.strip('\n') for item in todos]
+            for index, item in enumerate(new_todos):
+                item = item.capitalize()
+                print(f"{index + 1} - {item}",)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            todos[number - 1] = input("Enter new todo: ") + "\n"
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+        case 'complete':
+            number = int(input("Number of the todo to complete: "))
+            index = number - 1
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
+            message = f'Todo "{todo_to_remove}" was removed from the list.'
+            print(message)
+        case 'exit':
+            break
+        case _:
+            print("You entered an unknown command.")
+
+print("Bye!")
+
